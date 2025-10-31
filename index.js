cadf4ad6edca90f8c8dc6a7fdb9349f2dd56c6a4
vernecic <vidernecic1@gmail.com>
2025-10-31 17:25:28 +0100

    added users

--- index.js
+++ index.js
@@ -8,5 +8,31 @@ app.listen(PORT, () =>{
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/public/about.html')
+})
+
+const users = [
+    {
+        id: 1,
+        ime: 'Luka',
+        prezime: 'Modrić'
+    },
+    {
+        id: 2,
+        ime: 'Zoran',
+        prezime: 'Milanović'
+    },
+    {
+        id: 3,
+        ime: 'Dado',
+        prezime: 'Pršo'
+    }
+
+]
+app.get('/users', (req, res) => {
+    res.json(users)
 })
